878c93f320eb554e6f176531a13579139e5df783
1DreaM1 <dakristl123@gmail.com>
2020-02-21 17:22:44 +0100

    contacts update

--- contact.php
+++ contact.php
@@ -80,10 +80,8 @@
 					<div class="fh5co-contact-info">
 						<h3>Contact Information</h3>
 						<ul>
-							<li class="address">198 West 21th Street, <br> Suite 721 New York NY 10016</li>
-							<li class="phone"><a href="tel://1234567920">+ 1235 2355 98</a></li>
-							<li class="email"><a href="mailto:info@yoursite.com">info@yoursite.com</a></li>
-							<li class="url"><a href="http://gettemplates.co">gettemplates.co</a></li>
+							<li class="email"><a href="https://discord.gg/utfngQn">https://discord.gg/utfngQn</a></li>
+							<li class="url"><a href="https://bejoijo.ml">BejoIjo.ml</a></li>
 						</ul>
 					</div>
 
